956f38a8dbf31cace98863f93b5138ea8f2d659e
Harsh Anand <harshanandpro07@gmail.com>
2025-10-31 23:29:42 +0530

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBgl4fflARcELAaRtBTl6JrX7tFhhl9oq0",
-  authDomain: "arnazon-3c7a2.firebaseapp.com",
-  projectId: "arnazon-3c7a2",
-  storageBucket: "arnazon-3c7a2.firebasestorage.app",
-  messagingSenderId: "799157279471",
-  appId: "1:799157279471:web:11a482e84f64bd4af6d91a",
-  measurementId: "G-B039JSJM77"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 export const auth = getAuth(app);
-// 
-// project-799157279471
