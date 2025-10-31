dd10790a009d2cfff05871814acb08e6b925f40a
Harsh Anand <harshanandpro07@gmail.com>
2025-11-01 01:19:23 +0530

    bug fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+console.log('API Key:', import.meta.env.VITE_FIREBASE_API_KEY);
+console.log('Auth Domain:', import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
